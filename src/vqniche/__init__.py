f6718dc4f92cf52478404d81b1b1993e1460d14a
arpitdm <arpitdm@gmail.com>
2024-10-21 22:36:15 +0100

    update: moves original code files of vqgraph into their own folder and fixes relative import errors.

--- src/vqniche/__init__.py
+++ src/vqniche/__init__.py
@@ -1,8 +1,1 @@
-from . import (data_preprocess,
-				dataloader,
-				models,
-				train_and_eval,
-				train_student,
-				train_teacher,
-				utils,
-				vq)
+from . import vqgraph
---
+++ src/vqniche/vqgraph/__init__.py
@@ -0,0 +1,8 @@
+from . import (data_preprocess,
+				dataloader,
+				models,
+				train_and_eval,
+				train_student,
+				train_teacher,
+				utils,
+				vq)
