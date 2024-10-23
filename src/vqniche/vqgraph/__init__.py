7ab87e13e5dd7f20d511970735ea7316721bdc5b
arpitdm <arpitdm@gmail.com>
2024-10-23 13:05:54 +0100

    hotfix: updates relative imports within vqniche package

--- src/vqniche/vqgraph/__init__.py
+++ src/vqniche/vqgraph/__init__.py
@@ -1,8 +1,8 @@
-from . import (data_preprocess,
-				dataloader,
-				models,
-				train_and_eval,
-				train_student,
-				train_teacher,
-				utils,
-				vq)
+from .data_preprocess import *
+from .dataloader import load_data
+from .models import *
+from .train_and_eval import *
+from .train_student import *
+from .train_teacher import *
+from .utils import *
+from .vq import *
