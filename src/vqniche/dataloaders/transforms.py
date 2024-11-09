0cadfcd2c24004dbcfd1d15c586e03d71a71a0e4
arpitdm <arpitdm@gmail.com>
2024-11-09 13:05:21 +0000

    update: fixes docstrings; separates setting data keys from other data transforms

--- src/vqniche/dataloaders/transforms.py
+++ src/vqniche/dataloaders/transforms.py
@@ -4,84 +4,83 @@ from typing import Any, List
 from ..preprocessors.normalizers import normalize_by_read_depth
 
 
-def prepare_transforms(transform_list: List[str] = ['SetCustomData',
-                                                    'RandomNodeSplit',
-                                                    'NormalizeFeatures'],
-                        val_ratio: float = 0.1,
-                        test_ratio: float = 0.2,
-                        feature_key: str = 'x_cell_gene_counts',
-                        label_key: str = 'y_cell_types',
-                        edge_index_key: str = 'edge_index_spatial-delaunay',
-                        norm_method: str = 'read_depth'
-                        ) -> T.Compose:
+def init_data_transforms(
+        data_transform_names: List[str] = ['NormalizeFeatures',
+                                            'RandomNodeSplit'],
+        norm_method: str = 'read_depth',
+        val_ratio: float = 0.1,
+        test_ratio: float = 0.2,
+    ) -> List[T.BaseTransform]:
     """
-    Prepare a list of PyG transforms to be applied to the dataset.
+    Initialize a list of PyG transforms to be applied to the Data object.
 
     Parameters:
     ----------
-    - transform_list (List[str]): The list of transforms to be applied.
-    - val_ratio (float): The ratio of validation data.
-    - test_ratio (float): The ratio of test data.
-    - feature_key (str): The key for the node features to set for experiments.
-    - label_key (str): The key for the node labels to set for experiments.
-    - edge_index_key (str): The key for the edge index to set for experiments.
-    - norm_method (str): The method to use for normalization.
+    - data_transform_names: List[str])
+        The list of PyG transforms to initialize.
+    - norm_method: str
+        The method to use for normalization.
+    - val_ratio: float
+        The ratio of validation data.
+    - test_ratio: float
+        The ratio of test data.
 
     Returns:
     -------
-    - T.Compose: The composed PyG transforms.
+    - List[T.BaseTransform]
+        A list of PyG transforms to be applied to data.
 
-    Notes:
-    -----
-    - The order of transforms is important. SetCustomDataKeys must be the first transform if data.x, data.y, and data.edge_index are not set.
     """
-    transforms = []
-    for transform in transform_list:
-        if transform == 'RandomNodeSplit':
-            transforms.append(T.RandomNodeSplit(
+    data_transforms = []
+
+    for data_transform_name in data_transform_names:
+        if data_transform_name == 'NormalizeFeatures':
+            data_transforms.append(NormalizeFeatures(
+                                        feature_key='x',
+                                        norm_method=norm_method
+                                        )
+                            )
+
+        elif data_transform_name == 'RandomNodeSplit':
+            data_transforms.append(T.RandomNodeSplit(
                                         split='train_rest',
                                         num_val=val_ratio,
-                                        num_test=test_ratio)
+                                        num_test=test_ratio
+                                        )
                             )
-        elif transform == 'NormalizeFeatures':
-            transforms.append(NormalizeFeatures(norm_method=norm_method))
-        elif transform == 'SetCustomDataKeys':
-            transforms.append(SetCustomDataKeys(
-                                    feature_key=feature_key,
-                                    label_key=label_key,
-                                    edge_index_key=edge_index_key)
-                                    )
-        else:
-            raise ValueError(f"Transform {transform} not found.")
 
-    return T.Compose(transforms)
+        else:
+            raise ValueError(f"{data_transform_name} Transform not found.")
+
+    return data_transforms
 
 
 class NormalizeFeatures(T.BaseTransform):
     def __init__(self,
-                 norm_method: str = 'read_depth',
-                 feature_key: str = 'x'):
+            norm_method: str = 'read_depth',
+            feature_key: str = 'x'
+        ):
         """
         Normalize the features of the PyG Data object.
 
         Parameters:
         ----------
-        - norm_method (str): The method to use for normalization.
-        - feature_key (str): The key for the node features to normalize.
-
-        Returns:
-        -------
-        - None
+        - norm_method: str
+            The method to use for normalization.
+        - feature_key: str
+            The key for the node features that are to be normalized.
 
         Notes:
         -----
-        Ideally, the feature_key should be 'x' for the node features. The option exists to allow for flexibility.
+        Ideally, the feature_key should be 'x' for the node features. The option exists to pass a custom key to allow for flexibility during training, if required.
         """
         self.norm_method = norm_method
         self.feature_key = feature_key
 
-    def forward(self,
-                data: Any) -> Any:
+    def forward(
+            self,
+            data: Any
+        ) -> Any:
         """
         Normalize the features of the PyG Data object.
         """
@@ -94,32 +93,36 @@ class NormalizeFeatures(T.BaseTransform):
         return data
 
 
-class SetCustomDataKeys(T.BaseTransform):
-    def __init__(self,
-                 feature_key: str = 'x_cell_gene_counts',
-                 label_key: str = 'y_cell_types',
-                 edge_index_key: str = 'edge_index_spatial-delaunay'):
+class SetExperimentDataKeys(T.BaseTransform):
+    def __init__(
+            self,
+            feature_name: str = 'cell_gene_counts',
+            label_name: str = 'cell_types',
+            edge_index_name: str = 'spatial-delaunay'
+        ):
         """
-        Set custom data keys for the PyG Data object.
+        Set data.x, data.y, and data.edge_index keys for the PyG Data object from Experiment keys.
+        This is useful to control before starting training so we can choose on the fly which features, labels, and edge indices to use (e.g., cell gene counts for cell type classification, etc.)
 
         Parameters:
         ----------
-        - feature_key (str): The key for the node features to set.
-        - label_key (str): The key for the node labels to set.
-        - edge_index_key (str): The key for the edge index to set.
-
-        Returns:
-        -------
-        - None
+        - feature_name: str
+            The key for the node features to set.
+        - label_name: str
+            The key for the node labels to set.
+        - edge_index_name: str
+            The key for the edge index to set.
         """
-        self.feature_key = feature_key
-        self.label_key = label_key
-        self.edge_index_key = edge_index_key
+        self.feature_key = f"x_{feature_name}"
+        self.label_key = f"y_{label_name}"
+        self.edge_index_key = f"edge_index_{edge_index_name}"
 
-    def forward(self,
-                data: Any):
+    def forward(
+            self,
+            data: Any
+        ) -> Any:
         """
-        Set custom data keys for the PyG Data object.
+        Set Experiment data keys for the PyG Data object.
         """
         data.x = getattr(data, self.feature_key)
         data.y = getattr(data, self.label_key)
