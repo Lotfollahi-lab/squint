fdf10cc31131a3170f7aadc9b5162cbebcb07d80
arpitdm <arpitdm@gmail.com>
2025-04-04 02:14:28 +0100

    feature: adds functions for computing mmd statistics

--- src/vqniche/utils/metrics.py
+++ src/vqniche/utils/metrics.py
@@ -1,8 +1,12 @@
 import numpy as np
+import networkx as nx
 from sklearn.metrics import accuracy_score as sklearn_accuracy_score
+from typing import Optional
+import scipy.sparse as sp
 
 import torch
 import torch.nn.functional as F
+
 
 def accuracy_score(
         unnormalized_logits: torch.Tensor,
@@ -81,3 +85,153 @@ def get_similarity_stats(
     return {
         f'{prefix}_mean': similarities.mean().item(),
     }
+
+
+def build_reconstructed_adjacency_matrix(
+        pred_adj: torch.Tensor,
+    ) -> torch.Tensor:
+    """
+    Build the reconstructed adjacency matrix from the predicted adjacency matrix and the edge index.
+
+    Parameters
+    ----------
+    - pred_adj: torch.Tensor
+        The quantized matrix coming from the decoder module.
+        Dimensions: (num_nodes, decoder_embedding_dim)
+
+    Returns
+    -------
+    - reconstructed_adjacency_matrix: torch.Tensor
+        The reconstructed adjacency matrix.
+        Dimensions: (num_nodes, num_nodes)
+    """
+    reconstr_adj = torch.matmul(pred_adj.detach(), pred_adj.detach().t())
+    reconstr_adj = (reconstr_adj - reconstr_adj.min()) / (reconstr_adj.max() - reconstr_adj.min() + 1e-8)
+
+    reconstr_adj[reconstr_adj < 0.5] = 0
+    reconstr_adj[reconstr_adj >= 0.5] = 1
+
+    return reconstr_adj
+
+
+def compute_node_distributions(
+        G: nx.Graph
+    ) -> torch.Tensor:
+    """
+    Compute the local (node) clustering coefficient distribution given an adjacency matrix.
+
+    Parameters
+    ----------
+    - adjacency_matrix: torch.Tensor
+        The adjacency matrix.
+        Dimensions: (num_nodes, num_nodes)
+
+    Returns
+    -------
+    - node_degree_distribution: numpy.ndarray
+        The node degree distribution.
+        Dimensions: (num_nodes)
+    - clustering_coefficient_distribution: numpy.ndarray
+        The clustering coefficient distribution.
+        Dimensions: (num_nodes)
+    """
+    # compute the node degree distribution
+    node_degree_distribution = np.array(list(dict(G.degree()).values()))
+
+    # compute the local clustering coefficient for each node
+    clustering_coefficient_distribution = np.array(list(nx.clustering(G).values()))
+
+    # compute the 4-orbit count distribution
+    orbit_count_distribution = np.array(list(nx.four_cycles(G).values()))
+
+    return node_degree_distribution, clustering_coefficient_distribution, orbit_count_distribution
+
+
+def compute_spectral_density_distribution(
+        G: nx.Graph,
+        k: Optional[int] = 100,
+        n_bins: Optional[int] = 50,
+        density: Optional[bool] = True
+    ) -> np.ndarray:
+    """
+    Compute the spectral density distribution for a given graph.
+
+    Parameters
+    ----------
+    - G: nx.Graph
+        The graph.
+    - k: int
+        The number of eigenvalues to compute.
+    - n_bins: int
+        The number of bins to use for the histogram.
+    - density: bool
+        Whether to return the density of the histogram.
+
+    Returns
+    -------
+    - spectral_density: numpy.ndarray
+        The spectral density distribution.
+        Dimensions: (n_bins)
+    """
+    # compute the normalized Laplacian matrix
+    L = nx.normalized_laplacian_matrix(G)
+
+    # compute the eigenvalues of the Laplacian matrix
+    # note: eigvalsh returns a 1D array of sorted eigenvalues with multiplicity
+    # NOTE: maybe use shift-invert mode for better performance?
+    eigenvalues = sp.eigsh(
+                        A=L,
+                        k=k,
+                        which='SM',
+                        tol=1e-4,
+                        maxiter=1000,
+                        return_eigenvectors=False,
+                    )
+
+    # quantize eigenvalues into a probability density function
+    spectral_density, _ = np.histogram(
+        a=eigenvalues,
+        bins=n_bins,
+        density=density,
+    )
+
+    return spectral_density
+
+
+def compute_distribution_discrepancy(
+        input_distribution: np.ndarray,
+        target_distribution: np.ndarray,
+        method: Optional[str] = 'total_variation'
+    ) -> float:
+    """
+    Compute the discrepancy between two distributions.
+
+    Parameters
+    ----------
+    - input_distribution: numpy.ndarray
+        The input distribution.
+        Dimensions: (num_nodes)
+    - target_distribution: numpy.ndarray
+        The target distribution.
+        Dimensions: (num_nodes)
+    - method: str
+        The method to use to compute the discrepancy.
+        Options: 'total_variation', 'kl_divergence'
+
+    Returns
+    -------
+    - discrepancy: float
+        The discrepancy between the two distributions.
+    """
+    if method == 'total_variation':
+        return np.linalg.norm(
+            x=input_distribution - target_distribution,
+            ord=2
+        )
+    elif method == 'kl_divergence':
+        return np.sum(
+            a=input_distribution * np.log(input_distribution / target_distribution),
+            axis=None,
+        )
+    else:
+        raise ValueError(f"Invalid method: {method}")
