d1eb9e1b12e413410ba64b097252db5c4f2097d7
arpitdm <arpitdm@gmail.com>
2025-05-05 10:16:07 +0100

    feature: transform to concatenate gene counts with node embeddings. separate initializers for gene count and train transforms.

--- src/vqniche/dataset/transforms.py
+++ src/vqniche/dataset/transforms.py
@@ -1,35 +1,29 @@
-from torch_geometric.data import Data
-from typing import Optional, List
+from typing import Optional, List, Literal
 
 import scanpy as sc
 import anndata as ad
 
 import torch
+from torch_geometric.data import Data
 import torch_geometric.transforms as T
 
 from ..preprocessors.normalizers import normalize_by_read_depth, normalize_total_log1p
 
 
-def init_data_transforms(
-        data_transform_names: List[str] = [
-            'SubsetHVG',
-            'NormalizeFeatures',
-            'RandomNodeSplit',
-        ],
+def init_gene_count_transforms(
+        gene_count_transform_names: List[Literal['SubsetHVG', 'NormalizeFeatures']] = ['SubsetHVG'],
         n_genes: int = 1000,
         norm_method: str = 'total_log1p',
         target_size: int = 10_000,
         apply_CPM: bool = True,
-        val_ratio: float = 0.1,
-        test_ratio: float = 0.2,
     ) -> List[T.BaseTransform]:
     """
-    Initialize a list of PyG transforms to be applied to the Data object.
+    Initialize a list of PyG transforms to be applied to the raw cell gene counts matrix.
 
     Parameters:
     ----------
-    - data_transform_names: List[str])
-        The list of PyG transforms to initialize.
+    - gene_count_transform_names: List[Literal['SubsetHVG', 'NormalizeFeatures']]
+        The list of PyG transforms to initialize. Currently, only 'SubsetHVG' and 'NormalizeFeatures' are supported.
     - n_genes: int
         The number of highly variable genes to subset the data to.
     - norm_method: str
@@ -38,49 +32,66 @@ def init_data_transforms(
         The target size for normalization.
     - apply_CPM: bool
         If True, apply CPM normalization.
-    - val_ratio: float
-        The ratio of validation data.
-    - test_ratio: float
-        The ratio of test data.
 
     Returns:
     -------
     - List[T.BaseTransform]
         A list of PyG transforms to be applied to data.
 
+    Notes:
+    -----
+    - Transforms are not order-invariant.
+    - If 'SubsetHVG' is listed, it is applied before 'NormalizeFeatures'.
     """
-    data_transforms = []
+    gene_count_transforms = []
 
-    for data_transform_name in data_transform_names:
-        if data_transform_name == 'SubsetHVG':
-            data_transforms.append(
-                SubsetHVG(
-                    n_genes=n_genes
-                )
+    if 'SubsetHVG' in gene_count_transform_names:
+        gene_count_transforms.append(
+            SubsetHVG(
+                n_genes=n_genes
             )
-        elif data_transform_name == 'NormalizeFeatures':
-            data_transforms.append(
-                NormalizeFeatures(
-                    feature_key='x',
-                    norm_method=norm_method,
-                    target_size=target_size,
-                    apply_CPM=apply_CPM
-                )
+        )
+
+    if 'NormalizeFeatures' in gene_count_transform_names:
+        gene_count_transforms.append(
+            NormalizeFeatures(
+                norm_method=norm_method,
+                target_size=target_size,
+                apply_CPM=apply_CPM
             )
+        )
 
-        elif data_transform_name == 'RandomNodeSplit':
-            data_transforms.append(
-                T.RandomNodeSplit(
-                    split='train_rest',
-                    num_val=val_ratio,
-                    num_test=test_ratio
-                )
+    return gene_count_transforms
+
+
+def init_train_transforms(
+        train_transform_names: List[Literal['RandomNodeSplit']] = ['RandomNodeSplit'],
+        val_ratio: float = 0.1,
+        test_ratio: float = 0.1,
+    ) -> List[T.BaseTransform]:
+    """
+    Initialize a list of training-related PyG transforms to be applied to the Data object.
+
+    Parameters:
+    ----------
+    - train_transform_names: List[Literal['RandomNodeSplit']]
+        The list of PyG transforms to initialize. Currently, only 'RandomNodeSplit' is supported.
+    - val_ratio: float
+        Fraction of the data to set aside for validation.
+    - test_ratio: float
+        Fraction of the data to set aside for testing.
+    """
+    train_transforms = []
+    if 'RandomNodeSplit' in train_transform_names:
+        train_transforms.append(
+            T.RandomNodeSplit(
+                split='train_rest',
+                num_val=val_ratio,
+                num_test=test_ratio,
+                key='y'
             )
-
-        else:
-            raise ValueError(f"{data_transform_name} Transform not found.")
-
-    return data_transforms
+        )
+    return train_transforms
 
 
 class SubsetHVG(T.BaseTransform):
@@ -88,22 +99,32 @@ class SubsetHVG(T.BaseTransform):
             self,
             n_genes: int = 1000,
         ):
-        self.n_genes = n_genes
+        """
+        Subset the raw gene counts matrix to the top n_genes highly variable genes.
 
+        Parameters:
+        ----------
+        - n_genes: int
+            The number of highly variable genes to subset the data to.
+        """
+        self.n_genes = n_genes
 
     def forward(
             self,
             data: Data
         ) -> Data:
-        adata = ad.AnnData(data.x.numpy())
+        """
+        This transform is applied to the raw gene counts matrix.
+        """
+        adata = ad.AnnData(data.x_cell_gene_counts.numpy())
         sc.pp.highly_variable_genes(
             adata,
             flavor='seurat_v3',
             n_top_genes=self.n_genes,
             subset=True
         )
-        data.x = torch.from_numpy(adata.X).to(data.x.device)
-        data.num_features = data.x.shape[1]
+        # we manually set the data.x to the subsetted adata.X
+        data.x = torch.from_numpy(adata.X)
         print(f"SubsetHVG: Subsetted data to {data.num_features} features.")
         return data
 
@@ -112,7 +133,6 @@ class NormalizeFeatures(T.BaseTransform):
     def __init__(
             self,
             norm_method: str = 'read_depth',
-            feature_key: str = 'x',
             target_size: int = 10_000,
             apply_CPM: Optional[bool] = True
         ):
@@ -123,8 +143,6 @@ class NormalizeFeatures(T.BaseTransform):
         ----------
         - norm_method: str
             The method to use for normalization.
-        - feature_key: str
-            The key for the node features that are to be normalized.
         - target_size: int
             The target size for normalization.
         - apply_CPM: bool
@@ -135,7 +153,6 @@ class NormalizeFeatures(T.BaseTransform):
         Ideally, the feature_key should be 'x' for the node features. The option exists to pass a custom key to allow for flexibility during training, if required.
         """
         self.norm_method = norm_method
-        self.feature_key = feature_key
         self.target_size = target_size
         self.apply_CPM = apply_CPM
 
@@ -146,7 +163,13 @@ class NormalizeFeatures(T.BaseTransform):
         """
         Normalize the features of the PyG Data object.
         """
-        feature_data = getattr(data, self.feature_key)
+        # if SubsetHVG is applied, the feature data to normalize is data.x
+        if 'x' in data.keys():
+            feature_data = data.x
+        # if SubsetHVG is not applied, the feature data to normalize is the raw cell gene counts in data.x_cell_gene_counts
+        else:
+            feature_data = data.x_cell_gene_counts
+
         if self.norm_method == 'read_depth':
             feature_data = normalize_by_read_depth(
                             x=feature_data,
@@ -160,14 +183,17 @@ class NormalizeFeatures(T.BaseTransform):
                         )
         else:
             raise ValueError(f"Normalization method {self.norm_method} not found.")
-        setattr(data, self.feature_key, feature_data)
+
+        # the normalized feature data is stored in data.x
+        setattr(data, 'x', feature_data)
+
         return data
 
 
 class SetExperimentDataKeys(T.BaseTransform):
     def __init__(
             self,
-            feature_name: str = 'cell_gene_counts',
+            feature_names: List[Literal['X', 'U_lm_eigvecs', 'U_deepwalk', 'U_gosh']] = ['X'],
             label_name: str = 'cell_types',
             edge_index_name: str = 'spatial-delaunay'
         ):
@@ -177,16 +203,83 @@ class SetExperimentDataKeys(T.BaseTransform):
 
         Parameters:
         ----------
-        - feature_name: str
-            The key for the node features to set.
-        - label_name: str
+        - feature_names: List[Literal['X', 'U_lm_eigvecs', 'U_deepwalk', 'U_gosh']]
+            The keys for the node features to set.
+        - label_name: Literal['cell_types']
             The key for the node labels to set.
-        - edge_index_name: str
+        - edge_index_name: Literal['spatial-delaunay']
             The key for the edge index to set.
+
+        Notes:
+        -----
+        - X refers to the raw cell gene counts.
+        - U_lm_eigvecs, U_deepwalk, U_gosh refer to the unsupervised node embeddings generated by the corresponding methods.
+        - If feature_names contains multiple names, the features are concatenated along the feature dimension.
         """
-        self.feature_key = f"x_{feature_name}"
-        self.label_key = f"y_{label_name}"
-        self.edge_index_key = f"edge_index_{edge_index_name}"
+        self.feature_names = feature_names
+        self.label_name = label_name
+        self.edge_index_name = edge_index_name
+
+    def set_node_attributes(
+            self,
+            data: Data,
+        ) -> torch.Tensor:
+        """
+        Set the node attributes in the data object given the feature_name.
+
+        Parameters:
+        ----------
+        - data: Data
+            The data object to set the node attributes in.
+
+        Returns:
+        -------
+        - torch.Tensor
+            The node attributes.
+        """
+        feature_keys = []
+        for feature_name in self.feature_names:
+            if feature_name == 'X':
+                # if SubsetHVG and/or NormalizeFeatures is applied, the feature data is in data.x
+                if 'x' in data.keys():
+                    feature_keys.append('x')
+                # if SubsetHVG and/or NormalizeFeatures is not applied, the feature data is in data.x_cell_gene_counts
+                else:
+                    feature_keys.append('x_cell_gene_counts')
+            elif feature_name in ['U_lm_eigvecs', 'U_deepwalk', 'U_gosh']:
+                feature_keys.append(f"{feature_name}_{self.edge_index_name}")
+            else:
+                raise ValueError(f"Feature key {feature_name} not found in data.")
+
+        x = torch.cat(
+                [getattr(data, key) for key in feature_keys],
+                dim=1
+            )
+        return x
+
+    def set_node_labels(
+            self,
+            data: Data,
+        ) -> torch.Tensor:
+        """
+        Set the node labels in the data object given the label_name.
+        """
+        if f"y_{self.label_name}" in data.keys():
+            return getattr(data, f"y_{self.label_name}")
+        else:
+            raise ValueError(f"Label key {self.label_name} not found in data.")
+
+    def set_edge_index(
+            self,
+            data: Data,
+        ) -> torch.Tensor:
+        """
+        Set the edge index in the data object given the edge_index_name.
+        """
+        if f"edge_index_{self.edge_index_name}" in data.keys():
+            return getattr(data, f"edge_index_{self.edge_index_name}")
+        else:
+            raise ValueError(f"Edge index key {self.edge_index_name} not found in data.")
 
     def forward(
             self,
@@ -195,17 +288,18 @@ class SetExperimentDataKeys(T.BaseTransform):
         """
         Set Experiment data keys for the PyG Data object.
         """
-        data.x = getattr(data, self.feature_key)
-        data.y = getattr(data, self.label_key)
-        data.edge_index = getattr(data, self.edge_index_key)
+        data.x = self.set_node_attributes(data)
+        data.y = self.set_node_labels(data)
+        data.edge_index = self.set_edge_index(data)
+
         data.num_features = data.x.shape[1]
         data.num_classes = data.y.shape[1]
         data.num_nodes = data.x.shape[0]
         data.num_edges = data.edge_index.shape[1]
 
-        # delete extra features, labels, and edge indices from the data object to reduce memory footprint during training
+        # delete extra features, edge indices, and embeddings from the data object to reduce memory footprint during training
         for key in list(data.keys()):
-            if key.startswith('x_') or key.startswith('edge_index_'):
+            if key.startswith('x_') or key.startswith('edge_index_') or key.startswith('U_'):
                 delattr(data, key)
 
         return data
